a9aa432ae1ad302c956f8d3e438227049bb32fe0
Al Gar <AllGitOut@users.noreply.github.com>
2025-10-01 19:50:11 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/invertify/', // GitHub Pages repository path for AllGitOut account
+  base: '/Invertify/', // GitHub Pages repository path for AllGitOut account
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
